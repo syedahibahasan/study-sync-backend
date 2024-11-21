2b2e8ef7163efe7067392a93fe48220de6dfe3ed
syedahibahasan <hibahasan151214@gmail.com>
2024-11-21 15:52:17 -0800

    New changes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  enrolledCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course" }],
+  enrolledCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: "scrappedcourses" }],
   schedule: [
     {
       day: String,  // e.g., "Sunday"
